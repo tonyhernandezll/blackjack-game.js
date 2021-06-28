5e979e531cc9da5836cc254a382bd8207a143e9d
Tony <tonyhll1972@gmail.com>
2021-06-27 21:45:22 -0500

    created a random card function that will give us random cards eact time

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -8,6 +8,13 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+
+
+//created a function to get a random card, and we will set it as the value for first, second, and new card
+function getRandomCard() {
+
+  return Math.floor(Math.random() * 13) + 1;
+}
 
 function startGame() {
   renderGame();
@@ -33,7 +40,7 @@ function renderGame() {
 
 
 function newCard() {
-  let card = 6;
+  let card = getRandomCard();
   sum += card;
   // Push the card to the cards array
   cards.push(card);
