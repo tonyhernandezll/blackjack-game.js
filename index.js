6e3471ba5c3e3ecc091a182ff3ae4af7ccec11d2
Tony <tonyhll1972@gmail.com>
2021-06-29 20:08:28 -0500

    modify the random car function to account for the ace, jack, queen, and king

--- index.js
+++ index.js
@@ -13,7 +13,15 @@ let cardsEl = document.getElementById("cards-el");
 //created a function to get a random card, and we will set it as the value for first, second, and new card
 function getRandomCard() {
 
-  return Math.floor(Math.random() * 13) + 1;
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function startGame() {
